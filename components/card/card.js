a66a720cce981ae0803a5fbfff6d12f0b08547e2
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-07 17:23:36 +0200

    add characterCardComponent

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,26 @@
-export function createCharacterCard() {}
+export function createCharacterCard(props) {
+  const newCard = document.createElement("li");
+  newCard.classList.add("card");
+  newCard.innerHTML = `
+    <div class="card__image-container">
+      <img
+        class="card__image"
+        src="${props.image}"
+        alt="${props.name}"
+      />
+      <div class="card__image-gradient"></div>
+    </div>
+    <div class="card__content">
+      <h2 class="card__title">${props.name}</h2>
+      <dl class="card__info">
+        <dt class="card__info-title">Status</dt>
+        <dd class="card__info-description">${props.status}</dd>
+        <dt class="card__info-title">Type</dt>
+        <dd class="card__info-description">${props.type}</dd>
+        <dt class="card__info-title">Occurrences</dt>
+        <dd class="card__info-description">${props.episode.length}</dd>
+      </dl>
+    </div>`;
+
+  return newCard;
+}
