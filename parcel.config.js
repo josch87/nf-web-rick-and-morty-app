9be7df6e0c7087712e5c0b727282df8831c8e4bb
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-09 19:32:16 +0200

    delete parcel config

--- parcel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Konfigurationsoptionen für Parcel hier definieren
-  // ...
-  // ...
-  // Definiere den Asset-Glob für die Datei, die immer deployed werden soll
-  includeFiles: ["assets/magnifying-glass.png"],
-};
