df05ccc2537e0bba584656a2966987d8771306f0
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-09 18:41:52 +0200

    add parcel config

---
+++ parcel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Konfigurationsoptionen für Parcel hier definieren
+  // ...
+  // ...
+  // Definiere den Asset-Glob für die Datei, die immer deployed werden soll
+  includeFiles: ["/assets/magnifying-glass.png"],
+};
